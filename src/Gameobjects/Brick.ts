947a423620c4ea807e72b671df81f610e52534d8
Lars Beukenkamp <lars@beukenkamp.net>
2023-02-27 18:17:42 +0100

    Adds brick class

--- src/Gameobjects/Brick.ts
+++ src/Gameobjects/Brick.ts
@@ -1,7 +1,22 @@
 class Brick extends HTMLElement {
-    constructor(width: number, height: number) {
-        super();
+    private x : number
+    private y : number
+    private width: number
+    private height: number
 
+    constructor(x: number, y: number, width: number, height: number) {
+        super()
+
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+
+        this.style.
+    }
+
+    private draw() : void {
+        this.style.transform = `translate(${this.x}px, ${this.y}px)`
     }
 }
 window.customElements.define("brick-component", Brick as any)
--- src/game.ts
+++ src/game.ts
@@ -15,32 +15,6 @@ class Game {
         this.gameLoop()
     }
 
-    // private createGrid() {
-    //     let rows        : number = 7
-    //     let columns     : number = 12
-    //     let brickWidth  : number = 64
-    //     let brickHeight : number = 32
-
-    //     for (let row = 0; row < rows; row++) {
-
-    //         for (let column = 0; column < columns; column++) {
-
-    //             // plaats het grid met blokken in het midden van het scherm
-    //             let offsetX = (window.innerWidth - columns * brickWidth) / 2
-    //             let x = column * brickWidth + offsetX
-                
-    //             // en op de y-as 100px vanaf de top
-    //             let y = row * brickHeight + 100
-                
-    //             // Voeg op deze plek een nieuw blok toe aan het spel
-    //             console.log(`Place brick at (${x}, ${y})`)
-    //             let game = document.getElementsByTagName("game")[0]
-    //             game.appendChild(this)
-    //         }
-    //     }
-    // }
-
-
     private gameLoop() {
         this.paddle.update()
 
